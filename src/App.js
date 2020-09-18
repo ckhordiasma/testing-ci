a2e4a589e0c68d9da64ff3a1ff873b632556347f
Christopher Kodama <ckhordiasma@gmail.com>
2020-09-17 20:03:53 -0400

    modified app.js to see if CI is working

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Test additional code asdfasdf
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
